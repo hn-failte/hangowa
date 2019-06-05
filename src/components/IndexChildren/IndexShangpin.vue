3fad616d445102495d1176cba3f64b427315e445
tianAHHH <1337284290@qq.com>
2019-06-05 15:49:39 +0800

    首页渲染完成

--- src/components/IndexChildren/IndexShangpin.vue
+++ src/components/IndexChildren/IndexShangpin.vue
@@ -1,12 +1,14 @@
 <template>
-    <dl v-for="(item,index) in goodsList" :key="index">
-        <dt><router-link :to="" :src=""></router-link></dt>
-        <dd>v-for  good in item
-            <img src="../../assets/10_06124375466059094_240.jpg">
-            <p>攀枝花新鲜大贵妃芒紫芒果 新鲜当季青水果带箱5斤爱文吉禄包邮</p>
-            <p class="jiage">￥32.80</p>
-        </dd>
-    </dl>
+    <div>
+        <dl v-for="(item,index) in goodsList" :key="index">
+            <dt><router-link tag="img" :to="item.dt.data" v-lazy="item.dt.image"></router-link></dt>
+            <router-link tag="dd" :to="{name:'detail', query: {data: obj.goods_id, type: 'goods'}}" v-for="(obj,index) in item.dd" :key="1+index">
+                <img v-lazy="obj.goods_image">
+                <p>{{obj.goods_name}}</p>
+                <p class="jiage">{{obj.goods_promotion_price}}</p>
+            </router-link>
+        </dl>
+    </div>
 </template>
 
 
@@ -16,7 +18,7 @@ export default{
     name: "IndexShangpin",
         computed: {
             ...Vuex.mapState({
-                goodsList: state=>state.index.goodsList 
+            goodsList: state=>state.index.goodsList 
         }),
     }
 }
