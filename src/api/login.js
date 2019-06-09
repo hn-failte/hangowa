80c06184192da71ccfb8dd2c0111ac70ba0af8c4
hn-failte <failte@163.com>
2019-06-09 16:41:31 +0800

    修复详情页不展示商品图片的bug

--- src/api/login.js
+++ src/api/login.js
@@ -2,11 +2,11 @@ import Http from "@utils/http"
 
 export default {
     login: (name, pwd)=>{ //登录
-        return Http.post("http://localhost:3000/users/login", "name="+name+"&pwd="+pwd) //测试服务器
-        // return Http.post("http://47.112.216.148:3000/users/login", "name="+name+"&pwd="+pwd) //正式服务器
+        // return Http.post("http://localhost:3000/users/login", "name="+name+"&pwd="+pwd) //测试服务器
+        return Http.post("http://47.112.216.148:3000/users/login", "name="+name+"&pwd="+pwd) //正式服务器
     },
     register: (name, pwd)=>{ //注册
-        return Http.post("http://localhost:3000/users/register", "name="+name+"&pwd="+pwd) //测试服务器
-        // return Http.post("http://47.112.216.148:3000/users/register", "name="+name+"&pwd="+pwd) //正式服务器
+        // return Http.post("http://localhost:3000/users/register", "name="+name+"&pwd="+pwd) //测试服务器
+        return Http.post("http://47.112.216.148:3000/users/register", "name="+name+"&pwd="+pwd) //正式服务器
     }
 }
